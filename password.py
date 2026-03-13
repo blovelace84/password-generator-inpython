dd5c8bd8d546a9b62cdce865642e6cb81d94e4a5
Brittani Lovelace <blovelace84@gmail.com>
2026-03-13 15:30:55 -0400

    Update password.py

--- password.py
+++ password.py
@@ -1,11 +1,19 @@
+#imports to generate random passwords and store them in strings
+
 import random
 import string
 
+#write a function to generate a password
 def gen_password():
+    # my variable to store
     my_password = ""
+    while len(my_password) < 10:
 
-    for i in range(10):
-        my_password += random.choice(string.ascii_letters)
-        print(my_password)
+        for i in range(10):
+            my_password += random.choice(string.ascii_letters)
+            if i % 2 == 0:
+                my_password += random.choice(string.ascii_letters)
+                print(my_password)
+
 
 gen_password()
